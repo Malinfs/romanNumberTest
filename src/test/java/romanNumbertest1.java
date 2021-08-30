3dfdb07d5c3b0185bbe4430c182d71dff1aaee1f
Malinfs <maso084@student.kristiania.no>
2021-08-30 18:07:09 +0200

    green to number 15

--- src/test/java/romanNumbertest1.java
+++ src/test/java/romanNumbertest1.java
@@ -57,29 +57,33 @@ public class romanNumbertest1 {
     private String toRoman(int number) {
         String result="";
 
-        if (number>=10){
-            result="X";
-            number-=10;
-        }
-        if (number==9){
-            result="IX";
-            number-=9;
+        while (number> 0){
+            if (number>9 && number<19){
+                result+="X";
+                number-=10;
+            }
+            if (number==9){
+                result+="IX";
+                number-=9;
+            }
+
+            if (number>4 && number<9){
+                result +="V";
+                number-=5;
+            }
+
+            if (number==4){
+                result+="IV";
+                number -=4;
+            }
+            for (int i = 0; i < number; i++) {
+                result += "I";
+                number-=1;
+            }
+
         }
 
-        if (number>=5){
-            result ="V";
-            number-=5;
-        }
 
-        if (number==4){
-            result="IV";
-            number -=4;
-        }
-
-
-        for (int i = 0; i < number; i++) {
-            result += "I";
-        }
         return result;
     }
 
